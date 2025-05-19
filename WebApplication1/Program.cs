4a0845c8fafa89f3152292d23ba651752774c664
Bhavya Fultariya <bhavyafultariya675@gmail.com>
2025-05-19 12:21:17 +0530

    changes

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,19 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IPhoneService, PhoneService>();
 
 var app = builder.Build();
-
-app.Run(async (context) =>
-{
-await context.Response.WriteAsync("Run Middleware Component Works and Getting Response from First Middleware!");
-});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,6 +28,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Run Middleware Component
+// app.Run(async (context) =>
+// {
+// await context.Response.WriteAsync("Run Middleware Component Works and Getting Response from First Middleware!");
+// });
+
+// Register custom middleware
+// app.UseMiddleware<RequestLoggingMiddleware>();
+
 // app.UseSwaggerUI(c =>
 // {
 //     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
@@ -41,6 +45,33 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
